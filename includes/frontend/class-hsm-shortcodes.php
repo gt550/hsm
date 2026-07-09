cf4ce1f14cf91298bba33fbe182cd0fb461719cc
GT <gt550@yahoo.com>
2026-07-09 19:11:40 -0400

    Add current user email shortcode

--- includes/frontend/class-hsm-shortcodes.php
+++ includes/frontend/class-hsm-shortcodes.php
@@ -8,9 +8,22 @@ class HSM_Shortcodes {
 
     public static function init() {
         add_shortcode('hsm_custom_test', [__CLASS__, 'test_shortcode']);
+        add_shortcode('hsm_current_user_email', [__CLASS__, 'current_user_email']);
     }
 
     public static function test_shortcode() {
         return '<div class="hsm-test-box">HSM Custom Plugin is active.</div>';
     }
+
+    public static function current_user_email() {
+
+        if (!is_user_logged_in()) {
+            return '';
+        }
+
+        $user = wp_get_current_user();
+
+        return esc_html($user->user_email);
+    }
+    
 }
